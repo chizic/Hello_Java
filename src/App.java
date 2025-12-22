8d4b3e342e246a35eb0dd0ecbdd12137fbab75bc
Mracle_chizi <chizijesus045@gmail.com>
2025-12-22 20:38:20 -0300

    Adding more accomplished exercises to the Switch case. It's getting more exciting.

--- src/App.java
+++ src/App.java
@@ -12,6 +12,8 @@ public class App {
             2 - Calculate area of a square;
             3 - Calculate area of a rectangle;
             4 - Calculate age difference between two people.
+            5 - Generate times-table for a given number.
+            6 - Calculate IMC.
             """);
 
         var option = scanner.nextInt();
@@ -57,6 +59,59 @@ public class App {
                 var difference = (age1 > age2) ? age1 - age2 : age2 - age1;
                 System.out.printf("The difference between ages is %s years apart\n ", difference);
             }
+            case 5 -> {
+                // Generate times-table
+                System.out.println("Please type a number to generate the times-table");
+                var num = scanner.nextInt();
+                for (int i =1;i<11;i++){int result = num*i;
+                     System.out.printf("%s X %s = %s\n",num,i,result);
+                    }
+            }
+            case 6 ->{
+                //Calculate IMC(IMC = weight/(height * height))
+                System.out.println("type weight (using coma in decimal form, eg. 75,2).");
+                var weight = scanner.nextDouble();
+                System.out.println("type height (using coma in decimal form eg 1,65).");
+                var height = scanner.nextDouble();
+                var imc =weight/(height*height);
+
+                if (imc<=18.5){System.out.println("Underweight");}
+                else if (imc>=18.6&&imc<=24.9){System.out.println("Ideal weight");}
+                else if (imc>=25.0&&imc<=29.9){System.out.println("slightly over weight");}
+                else if (imc>=30.0&&imc<=34.9){System.out.println("Obesity 1");}
+                else if (imc>=35.0&&imc<=39.9){System.out.println("Obesity 2");}
+                else if (imc>=40){System.out.println("Obesity 3");}
+                else {System.out.println("invalid IMC");}
+            }
+            case 7 -> {
+                //Calculate odd or even numbers between two numbers
+                System.out.println("Type a number");
+                var number1=scanner.nextInt();
+                System.out.println("Type a bigger number");
+                var number2  =scanner.nextInt();
+                System.out.println("Type 1 for even numbers\n Type 2 for odd numbers ");
+                var tipo = scanner.nextInt();
+                switch (tipo){
+                    case 1 ->{
+                        while (number2>=number1){
+                            if (number2%2==0){
+                                System.out.println(number2);
+                            }
+                            number2--;
+                        }
+
+                    }
+                    case 2 ->{
+                        while (number2>=number1){
+                            if (number2%2==1){
+                                System.out.println(number2);
+                            }
+                            number2--;
+                        }
+                    }
+                }
+            }
+            default -> System.out.println("Invalid option, please select a valid number.");
         }
 
         
